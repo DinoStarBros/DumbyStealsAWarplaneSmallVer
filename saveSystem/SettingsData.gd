bbacea20991f6a46671bd1edd0d0db0b1f8d70d7
DinoStarBros <lmatthewalven@gmail.com>
2025-12-26 18:54:39 +0800

    a

--- saveSystem/SettingsData.gd
+++ saveSystem/SettingsData.gd
@@ -2,9 +2,9 @@ extends RefCounted
 ## Contains the data of the settings menu that gets saved & loaded
 class_name SettingsData
 
-var master_volume : float = 0.75
-var music_volume : float = 0.75
-var sfx_volume : float = 0.75
+var master_volume : float = 0.0
+var music_volume : float = 0.0
+var sfx_volume : float = 0.0
 
 var screen_shake_value : bool = true
 var frame_freeze_value : bool = true
